9f73f58261d8e290ad78ad78b7c617d630dfe864
Jacob Latz <jlatz53@gmail.com>
2017-03-27 14:14:40 -0400

    Syncing

--- src/application/Main.java
+++ src/application/Main.java
@@ -248,7 +248,6 @@ public class Main extends Application {
 
 	public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException{
 		con = DriverManager.getConnection(url, user, password);
-		System.out.println("Database connected sucessfully");
 
 		launch(args);
 	}
